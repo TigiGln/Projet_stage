93a72ff2373dd2614ca7e1ef3c47387841753f3b
TigiGln <thierry.galliano@etu.univ-amu.fr>
2021-04-07 14:09:50 +0200

    ajout outil perl

--- Code_perl/outils_recherche_pmid.pl
+++ Code_perl/outils_recherche_pmid.pl
@@ -12,10 +12,6 @@ if ( !$ARGV[0] )
 	{
 		print "Argument PubmedID not found!\n$!" and die;
 	}
-#else
-#	{
-#		print $ARGV[0] . "\n";
-#	}
 my $pubmedid = $ARGV[0];
 
 # construction de la requête de recherche
@@ -23,29 +19,21 @@ my $base = 'http://www.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=Pubmed&ret
 
 # récupération du résultat comme si vous l'aviez ouvert dans votre navigateur
 my $search = get($base.$pubmedid);
-#print "output: " . $search . "\n";
-#print "\n";
 
-#$output =~ s/[\r\n] +/ /g;
+$output =~ s/[\r\n] +/ /g;
 
-#print "output regex: " . $output . "\n";
 my $WebEnv = $1 if ($search =~ m/<WebEnv>([^"]+)<\/WebEnv>/);
-#print $WebEnv . "\n";
-#if($WebEnv eq '')
-#	{
-#	 print "Error no WebEnv returned!";
-#	} 
-#else
-#	{
-#	 print $WebEnv;
-#	}
+if($WebEnv eq '')
+	{
+	 print "Error no WebEnv returned!";
+	} 
 # construction de la requête de recherche
 my $base1 = "http://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?rettype=abstract&retmode=xml&db=Pubmed&query_key=1&WebEnv=" . $WebEnv;
 my $url = $base1 . "&usehistory=y&term=" . $pubmedid;
 
 # récupération du résultat comme si vous l'aviez ouvert dans votre navigateur
 my $output = get($url);
-#print "$output \n";
+
 $output =~ s/[\r\n] / /g;
 $output =~ s/'//g;
 
@@ -59,13 +47,11 @@ my $authorList = '';
 my $authors = '';
 if ($output =~ /<AuthorList[^>]*>(.+?)<\/AuthorList>/g)
 	{
-		#print $output;
 		$authors = $1;
 	}
 while ($authors =~ /<Author[^>]*>(.+?)<\/Author>/g)
 	{
 		my $tempString = $1;
-		#print $tempString . "\n";
 		my $foreName = "";
 		my $name = "";
 		if ($tempString =~ m/<Initials>(.+?)<\/Initials>/)
@@ -105,7 +91,6 @@ while ($abstract =~ /<AbstractText[^>]*>(.+?)<\/AbstractText>/g)
 	}
 
 $abstractList =~ s/<.+?>//g;
-#$abstract =~ s/[\r\t\n]//g;
 print "PMID: " . $pubmedid . "\n";
 print "DOI: " . $doi . "\n\n";
 print "Titre: " . $title . "\n\n";
