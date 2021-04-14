b047c382ad73660b2118fca2471d4577aede2000
TigiGln <thierry.galliano@etu.univ-amu.fr>
2021-04-14 13:32:32 +0200

    ajout POO

---
+++ POO/start_session.php
@@ -0,0 +1,20 @@
+<?php
+require "class_connexion.php";
+session_start();
+
+if (!isset($_SESSION["connexion"]))
+{
+    $connexion = new Connexion("localhost", "stage", "thierry", "Th1erryG@llian0");
+    $_SESSION["connexion"] = $connexion;
+
+    echo "Actualiser la page";
+}
+else
+{
+    echo "<pre>";
+    var_dump($_SESSION["connexion"]); // On affiche les infos concernant notre objet.
+    echo "</pre>";
+}
+
+#session_destroy()
+?>
