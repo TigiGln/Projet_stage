693dbcb10a25d6696df7e17e005ab72872f3ed91
TigiGln <thierry.galliano@etu.univ-amu.fr>
2021-04-16 15:30:24 +0200

    ajout insertion simple

---
+++ test_insertion_simple/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html >
+<html lang="en">
+	<head>
+        <meta charset="utf-8" />
+        <title>Page de recherche d'article</title>
+    </head>
+    <body>
+        <form method="get" action="result.php" enctype="multipart/form-data">
+            <p>
+                <textarea name="pmid" id="pmid" cols="50" rows="4"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="Start search">
+            </p>
+        </form>
+    </body>
+</html>
