3f6b51d56c28fa52cbb46145bdd380f25bfb012b
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2021-04-19 18:05:00 +0200

    added parameters

--- readArticle.php
+++ readArticle.php
@@ -7,11 +7,15 @@ include('./views/menu.php');
 <!-- show one article to test-->
 <div class="p-4 w-75">
 <?php
-include('./utils/FromPMCID.php');
-?>
-</div>
-<?php
-//champ WYSIWYG
-include('./utils/WYSIWYG/wysiwyg.html');
+if(isset($_GET['PMCID'])) {
+	$PMCID = $_GET['PMCID'];//"7934857"; //"7531976"; "7934857"; 7857568
+	include('./utils/FromPMCID.php');
+	echo '</div>';
+	include('./utils/WYSIWYG/wysiwyg.html');
+} else {
+	echo '<div class="alert alert-danger" role="alert">
+			This page need an argument: ?PMCID=NUM
+		</div>';
+}
 include('./views/footer.html');
 ?>
