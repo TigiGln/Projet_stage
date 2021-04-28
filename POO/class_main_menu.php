190667e5e2560e17835b4185c788f52c551e24f0
TigiGln <thierry.galliano@etu.univ-amu.fr>
2021-04-28 17:33:58 +0200

    modif footer

--- POO/class_main_menu.php
+++ POO/class_main_menu.php
@@ -52,19 +52,19 @@ class MainMenu {
 
         if($this->My_Tasks) {
             if ($this->position == "to_treat") { $this->position = "My_Tasks"; }
-            $html = $this->writeOne($html, 'My_Tasks', '/Projet_stage/trie_table_statut/page_table.php', "?status=to_treat");
+            $html = $this->writeOne($html, 'My_Tasks', '/Projet_stage/trie_table_statut/page_table.php', "?status=2");
         }
         if($this->Open_Tasks) {
             if ($this->position == "undefined") { $this->position = "Open_Tasks"; }
-            $html = $this->writeOne($html, 'Open_Tasks', '/Projet_stage/trie_table_statut/page_table.php', "?status=undefined");
+            $html = $this->writeOne($html, 'Open_Tasks', '/Projet_stage/trie_table_statut/page_table.php', "?status=1");
         }
         if($this->Processed_Tasks) {
             if ($this->position == "treat") { $this->position = "Processed_Tasks"; }
-            $html = $this->writeOne($html, 'Processed_Tasks', '/Projet_stage/trie_table_statut/page_table.php', "?status=treat");
+            $html = $this->writeOne($html, 'Processed_Tasks', '/Projet_stage/trie_table_statut/page_table.php', "?status=3");
         }
         if($this->Rejected_Tasks) {
             if ($this->position == "reject") { $this->position = "Rejected_Tasks"; }
-            $html = $this->writeOne($html, 'Rejected_Tasks', '/Projet_stage/trie_table_statut/page_table.php', "?status=reject");
+            $html = $this->writeOne($html, 'Rejected_Tasks', '/Projet_stage/trie_table_statut/page_table.php', "?status=4");
         }
         if($this->Insertion) {
             $html = $this->writeOne($html, 'Insertion', '/Projet_stage/insertion/form.php', "");
