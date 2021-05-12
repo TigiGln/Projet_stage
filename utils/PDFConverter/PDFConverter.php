3f7c7007017d0ee0dd8aae7d40fd3d853f9c09d5
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2021-05-12 16:11:05 +0200

    began to work on pdf converter

--- utils/PDFConverter/PDFConverter.php
+++ utils/PDFConverter/PDFConverter.php
@@ -1,7 +1,7 @@
 <?php
     /**
      * 
-     * PDF2XML
+     * PDFConverter
      * 
      * Created on Wed May 12 2021
      * Latest update on Wed May 12 2021
@@ -10,9 +10,9 @@
      */
 
     //Prepare request
-    if(!isset($_GET['PATH']) && !isset($_GET['URL'])) {
+    if(!isset($_GET['PATH']) && !isset($_GET['URL']) && !isset($_GET['type'])) {
         echo '<div class="alert alert-danger" role="alert">
-            This page need an argument: ?PATH=folder&URL=URL<br>(&print to print the article).
+            This page need an argument: ?PATH=folder&URL=URL&type=[HTML/XML/TEXT]<br>(&print to print the article).
         </div>';
         exit(10);
     }
@@ -20,8 +20,13 @@
     $url = urldecode($_GET['URL']);
     $file = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $url);
     $file = mb_ereg_replace("([\.]{2,})", '', $file);
-    $path = $_GET['PATH']."/pdf/".$file.".pdf";
+    $path = $_GET['PATH'].$file;
 
     file_put_contents($path, fopen($url, 'r'));
     
+    if(file_exists("convert.py")) {
+        //$data = shell_exec("python ./convert.py ".$path." ".$_GET['type']);
+        //var_dump($data);
+        //unlink($path);
+    }
 ?>
